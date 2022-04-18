b6c7113a6a8200276632c09d4058db427e6c9991
Tucker <novaflash321@gmail.com>
2022-04-18 16:00:53 -0400

    test

--- Test/SLBLTest.cs
+++ Test/SLBLTest.cs
@@ -1,2 +1,42 @@
 using Xunit;
 using JAModel;
+using Moq;
+using JAConsoleBL;
+using JAConsoleDL;
+using System.Threading.Tasks;
+namespace Test;
+
+public class SLBLTests
+{
+    [Fact]
+    public async Task CreateShopItem()
+    {
+        var mockDL = new Mock<IRepo>();
+
+        ShopItem testItem = new ShopItem
+        
+        {
+            Name = "Test Name",
+            TypeOfFood = "Test Type"
+        };
+
+        ShopItem expectedItem = new ShopItem
+        {
+            Id = 8,
+            Name = "Test Name",
+            TypeOfFood = "Test Type",
+            Quantity = 20,
+            StoreID = 1,
+            Price = 0.50f
+        };
+
+        ConsoleProjBL bl = new ConsoleProjBL(mockDL.Object);
+
+        Task test = bl.CreateNewFoodItemAsync(testItem);
+        //ShopItem endItem = bl.CreateNewFoodItemAsync(testItem);
+
+        Assert.Equal(expectedItem.Name, testItem.Name);
+        Assert.Equal(expectedItem.TypeOfFood, testItem.TypeOfFood);
+
+    }
+}
